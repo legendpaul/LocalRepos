fdf43876b207bc3406dc87a7fcb78c8e5c57aa4c
Paul <legendpaulr@hotmail.com>
2025-12-03 09:51:05 +0000

    Ensure LocalRepo.bat continues after npm commands

--- LocalRepo.bat
+++ LocalRepo.bat
@@ -4,14 +4,12 @@ pushd "%~dp0"
 
 if not exist node_modules (
   echo Installing dependencies...
-  npm install
+  call npm install
 )
 
 echo Starting LocalRepos development server...
+echo This window will remain open after startup. Use Ctrl+C to stop the server when you're finished.
 call npm start
-
-echo.
-echo If the server stopped unexpectedly, review the logs above.
 pause
 
 popd
